61a6a165a082d13998f954f5aa67661fd5dd1a05
Benjamin Saks <saksben2@gmail.com>
2024-04-06 16:00:18 -0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
+import "./globals.css";
+import "./HomePage.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+  <div>Header</div>
+  <Component {...pageProps} />
+  </>
+
 }
