0b5d763ef44a558978ac779b0c60ea6071e12e35
Benjamin Saks <saksben2@gmail.com>
2024-04-07 16:02:59 -0700

    add server-side rendering, only needs mongodb connection string

---
+++ pages/api/products.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+async function getProducts() {
+  const uri = process.env.MONGODB_CONNECTION_STRING;
+  const dbName = process.env.MONGO_DB_NAME;
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const collection = client.db(dbName).collection("albums");
+
+  const albums = await collection.find({}).toArray();
+
+  client.close();
+
+  return albums;
+}
+
+export default async function handler(req, res) {
+    const albums = await getProducts()
+
+    res.status(200).json(albums)
+}
--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -1,23 +1,79 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import { MongoClient } from "mongodb";
 
-import ProductDetails from '../../components/product-details'
-import Cart from '../../components/cart'
-import styles from './Catalog.module.scss'
-import products from './products.json'
+import ProductDetails from "../../components/product-details";
+import Cart from "../../components/cart";
+import styles from "./Catalog.module.scss";
+import products from "./products.json"; // Remove this if you have an api and db
 
-function Catalog() {
-  const [cart, setCart] = useState({ products: [] })
+// Server side rendering (needs api to work)
+// export async function getServerSideProps(context) {
+//   async function getProducts() {
+//     const uri = process.env.MONGODB_CONNECTION_STRING;
+//     const dbName = process.env.MONGO_DB_NAME;
+//     const client = await MongoClient.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     const collection = client.db(dbName).collection("albums");
+
+//     const albums = await collection.find({}).toArray();
+
+//     client.close();
+
+//     return albums;
+//   }
+//   const productsFromDb = await getProducts();
+
+//   return { props: { products: productsFromDb } };
+// }
+
+// Static generation + incremental static generation
+export async function getStaticProps() {
+  async function getProducts() {
+    const uri = process.env.MONGODB_CONNECTION_STRING;
+    const dbName = process.env.MONGO_DB_NAME;
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const collection = client.db(dbName).collection("albums");
+
+    const albums = await collection.find({}).toArray();
+
+    client.close();
+
+    return albums;
+  }
+  const productsFromDb = await getProducts();
+
+  // Revalidate means it will trigger another static regeneration (refresh the content) every x seconds
+  return { props: { products: productsFromDb }, revalidate: 60 };
+}
+
+function Catalog({ products }) {
+  const [cart, setCart] = useState({ products: [] });
+  // const [products, setProducts] = useState([]); // If you have an api and db
+
+  // If you have an api and db
+  // function fetchProducts() {
+  //   fetch("/api/products")
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data));
+  // }
+  //
+  // useEffect(() => fetchProducts(), []);
 
   function addToCart(product) {
-    const newCart = { _id: cart._id }
-    newCart.products = [...cart.products, { ...product }]
-    setCart(newCart)
+    const newCart = { _id: cart._id };
+    newCart.products = [...cart.products, { ...product }];
+    setCart(newCart);
   }
 
   function removeItemFromCart(product) {
-    const newCart = { _id: cart._id }
-    newCart.products = cart.products.filter(item => item !== product)
-    setCart(newCart)
+    const newCart = { _id: cart._id };
+    newCart.products = cart.products.filter((item) => item !== product);
+    setCart(newCart);
   }
 
   return (
@@ -32,14 +88,17 @@ function Catalog() {
               </li>
             ))}
           </ul>
-        </div >
+        </div>
         <div className={styles.rightSidebar}>
           <h2>Cart</h2>
-          <Cart cartItems={cart.products} removeItemFromCart={removeItemFromCart} />
+          <Cart
+            cartItems={cart.products}
+            removeItemFromCart={removeItemFromCart}
+          />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Catalog
+export default Catalog;
